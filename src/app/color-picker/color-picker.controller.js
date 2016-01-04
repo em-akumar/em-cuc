6d58ebdc47c9541296e9dfe2f4295b7c77bcb8cc
em-sghosh <subhadip.ghosh@elliemae.com>
2016-01-04 19:26:56 +0530

    color picker issue fix

--- src/app/color-picker/color-picker.controller.js
+++ src/app/color-picker/color-picker.controller.js
@@ -3,18 +3,6 @@ class ColorPickerController {
   /* @ngInject */
   constructor(ColorPickerService) {
     this.label = 'ColorPicker Controller !!';
-    this.colorPickerService = ColorPickerService;
-
-    angular.element(document.querySelector('.dropdown-menu.color-menu')).on('click', function(e) {
-      e.stopPropagation();
-    });
-    // this.initialize();
-  }
-
-  initialize() {
-    this.colorPickerService.resolvePromise().then((response) => {
-      this.data = response.data;
-    });
   }
 }
 
