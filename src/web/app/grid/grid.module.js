c61f1929bac6a3af9a1098927c91f2a1f139029a
em-sghosh <subhadip.ghosh@elliemae.com>
2016-01-27 14:37:26 +0530

    grid resize issue solved

--- src/web/app/grid/grid.module.js
+++ src/web/app/grid/grid.module.js
@@ -3,7 +3,7 @@ import {angular} from 'ems';
 import gridComponent from './grid.component';
 import GridService from './grid.service';
 
-let gridModule = angular.module('grid', ['ui.grid', 'ui.grid.moveColumns', 'ui.grid.pagination', 'ui.grid.selection', 'ui.grid', 'ui.grid.edit', 'ui.grid.exporter', 'ui.grid.resizeColumns','ui.grid.saveState','ui.grid.moveColumns']);
+let gridModule = angular.module('grid', ['ui.grid', 'ui.grid.moveColumns', 'ui.grid.pagination', 'ui.grid.selection', 'ui.grid', 'ui.grid.edit', 'ui.grid.exporter', 'ui.grid.resizeColumns','ui.grid.saveState','ui.grid.moveColumns','ui.grid.autoResize']);
 
 gridModule.config(($stateProvider) => {
   $stateProvider.state('grid', {
