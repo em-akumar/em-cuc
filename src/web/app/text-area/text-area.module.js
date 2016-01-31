725c49cc54fcd6737fce8b34aadef9402186f03b
em-akumar <adarsh.kumar@elliemae.com>
2016-01-30 21:51:48 -0800

    Updated the cuc demo pages

--- src/web/app/text-area/text-area.module.js
+++ src/web/app/text-area/text-area.module.js
@@ -3,6 +3,10 @@ import {angular, uiRouter} from 'ems';
 import textAreaComponent from './text-area.component';
 import TextAreaService from './text-area.service';
 
+import textAreaSize from './templates/text-area-size.html';
+import textAreaStates from './templates/text-area-states.html';
+import textAreaResize from './templates/text-area-resize.html';
+import textAreaLabel from './templates/text-area-label.html';
 let textAreaModule = angular.module('textArea', [uiRouter]);
 
 textAreaModule.config(($stateProvider) => {
@@ -12,6 +16,26 @@ textAreaModule.config(($stateProvider) => {
   });
 });
 
+textAreaModule.directive('textAreaSize', () => {
+  return {
+    template: textAreaSize
+  };
+});
+textAreaModule.directive('textAreaStates', () => {
+  return {
+    template: textAreaStates
+  };
+});
+textAreaModule.directive('textAreaResize', () => {
+  return {
+    template: textAreaResize
+  };
+});
+textAreaModule.directive('textAreaLabel', () => {
+  return {
+    template: textAreaLabel
+  };
+});
 textAreaModule.directive('textAreaComponent', textAreaComponent);
 textAreaModule.service('TextAreaService', TextAreaService);
 
