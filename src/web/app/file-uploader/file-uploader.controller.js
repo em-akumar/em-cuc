d2c95cde26bacbe1b443008bb41ed6413ebea807
SQi <simone.qi@elliemae.com>
2016-03-10 13:52:36 -0800

    cut off duplicated code and format code

--- src/web/app/file-uploader/file-uploader.controller.js
+++ src/web/app/file-uploader/file-uploader.controller.js
@@ -4,14 +4,12 @@ class FileUploaderController {
     this.fileUploaderService = FileUploaderService;
     this.fileUploaderRender();
     this.$http = $http;
-    this.fileDataGroup = [{"fileName": "file1", "fileSize": 20000, uploadDate: "05/09/2015", uploadUser: "Simone Qi"}];
-    this.initialize();
+    this.fileDataGroup = [];
   }
 
   fileUploaderRender() {
-    let configUploadUrl = 'http://10.112.104.13:85/v1/formbuilder/importpackage';
-
-    let fileFilterErrorMessage = '! Upload failed: Invalid file extension.',
+    let configUploadUrl = 'http://10.112.104.13:85/v1/formbuilder/importpackage',
+      fileFilterErrorMessage = '! Upload failed: Invalid file extension.',
       maxFileNameLength = '5120',
       fileNameLengthErrorMessage = 'File name length is greater than 100 characters.',
       dropDirectionMsg = 'Drop files to upload or  ',
@@ -19,6 +17,7 @@ class FileUploaderController {
       supportText = '[Supported File Format: .jpeg, .jpg, .png]',
       fileMaxSize = '5120',   //  size in KBs
       invalidFileMsg = '! Upload failed: Invalid file extension.';
+
     let fancyOptions = {
       controlId: 'fancyUploader',
       url: configUploadUrl,
@@ -37,6 +36,7 @@ class FileUploaderController {
         this.fileUploadComplete(e);
       }
     };
+
     let options = {
       controlId: 'divMultiFileUpload',
       spikeControlId: 'spikeUploader',
@@ -83,7 +83,7 @@ class FileUploaderController {
   }
 
   fileUploadComplete(e) {
-    var fileData = {} ;
+    var fileData = {};
     fileData.fileName = e.file.name
     fileData.fileSize = e.file.size;
     fileData.uploadDate = e.file.lastModifiedDate;
@@ -91,65 +91,6 @@ class FileUploaderController {
     console.log(fileData);
     this.fileDataGroup.push(fileData);
     console.log(this.fileDataGroup);
-    this.populateFileGrid(this.fileDataGroup);
-  }
-
-  populateFileGrid(fileDataGroup) {
-    this.fileGridOptions.data = fileDataGroup;
-  }
-
-  initialize() {
-
-    this.fileGridOptions = this.getGridConfig();
-    this.fileGridOptions.data = this.fileDataGroup;
-    //this.fileUploaderService.resolvePromise().then((response) => {
-    //  this.data = response.data;
-    //});
-  }
-
-
-  getGridConfig() {
-    return {
-      rowHeight: 40,
-      enableRowSelection: false,
-      enableRowHeaderSelection: false,
-      noUnselect: true,
-      columnDefs: [
-        {
-          field: 'fileName',
-          displayName: 'File Name',
-          width: '20%',
-          enableColumnMenu: false,
-          cellEditableCondition: false,
-          cellTemplate: '<div ><img src="" class="file-image" alt="img"/><a href="{{row.entity[\'url\']}}">{{row.entity[col.field]}}</a></div>'
-        },
-        {
-          field: 'uploadDate',
-          enableColumnMenu: false,
-          displayName: 'Upload Date',
-          width: '16%',
-          cellTemplate: '<div >{{row.entity[col.field]}}</div>',
-          cellEditableCondition: false
-        },
-        {
-          field: 'fileSize',
-          width: '12%',
-          enableColumnMenu: false,
-          type: 'number',
-          displayName: 'File Size',
-          cellEditableCondition: false,
-          cellTemplate: '<div >{{row.entity[col.field]}}</div>',
-        },
-        {
-          field: 'uploadUser',
-          width: '15%',
-          enableColumnMenu: false,
-          displayName: 'System Image',
-          cellEditableCondition: false,
-          cellTemplate: '<div>{{row.entity[col.field]}}</div>'
-        }
-      ]
-    };
   }
 }
 
