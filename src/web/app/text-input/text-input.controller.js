6fbf694e3d20cfe0a86e317fd8bac6cd444908a6
Sandip Kumbhar <Sandip.Kumbhar@elliemae.com>
2016-04-12 16:36:47 +0530

    lock input button changes

--- src/web/app/text-input/text-input.controller.js
+++ src/web/app/text-input/text-input.controller.js
@@ -3,6 +3,14 @@ class TextInputController {
   constructor(TextInputService) {
     this.textInputService = TextInputService;
     // this.initialize();
+    this.change_event = ($event) => {
+      console.log(angular.element($event.srcElement).hasClass('locked'));
+      if (angular.element($event.srcElement).hasClass('locked')) {
+        angular.element($event.srcElement).removeClass('locked').addClass('active').next('div').find('input').attr('readonly', false);
+      } else {
+        angular.element($event.srcElement).removeClass('active').addClass('locked').next('div').find('input').attr('readonly', true);
+      }
+    }
   }
   initialize() {
     this.textInputService.resolvePromise().then((response) => {
