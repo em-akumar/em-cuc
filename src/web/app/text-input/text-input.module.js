dd0421275dfa458e555358b888f39c5a222b5ee2
unknown <pshiral@XORIANTDEV-062.Corp.Elmae>
2016-05-12 19:12:22 +0530

    changes for decimal directive, ssn and color picker issue;

--- src/web/app/text-input/text-input.module.js
+++ src/web/app/text-input/text-input.module.js
@@ -80,55 +80,5 @@ textInputModule.directive('textInputLock', () => {
   };
 });
 
-textInputModule.directive('decimalOnly', function () {
-    return {
-      restrict: 'EA',
-        require: 'ngModel',
-        link: function (scope, element, attrs, ngModel) {
-           scope.$watch(attrs.ngModel, function(newValue, oldValue) {
-             var spiltArray = String(newValue).split("");
-
-              /*To allow only positive decimals*/
-              if(attrs.allowNegative == "false") {
-                if(spiltArray[0] == '-') {
-                  newValue = newValue.replace("-", "");
-                  ngModel.$setViewValue(newValue);
-                  ngModel.$render();
-                }
-             }
-
-              /*to restrict number of digits after decimal points*/
-              if (attrs.decimalUpto == 0) {
-                if (newValue) {
-                  newValue = newValue.replace(".", "");
-                  console.log(newValue);
-                  ngModel.$setViewValue(newValue);
-                  ngModel.$render();
-                }
-
-              }else {
-                   var n = String(newValue).split(".");
-                   if(n[1]) {
-                      var n2 = n[1].slice(0, attrs.decimalUpto);
-                      newValue = [n[0], n2].join(".");
-                      ngModel.$setViewValue(newValue);
-                      ngModel.$render();
-                   }
-              }
-
-
-              if (spiltArray.length === 0) return;
-              if (spiltArray.length === 1 && (spiltArray[0] == '-' || spiltArray[0] === '.' )) return;
-              if (spiltArray.length === 2 && newValue === '-.') return;
-
-                /*Check it is number or not.*/
-                if (isNaN(newValue)) {
-                  ngModel.$setViewValue(oldValue || '');
-                  ngModel.$render();
-                }
-            });
-        }
-    };
-});
 
 export default textInputModule;
