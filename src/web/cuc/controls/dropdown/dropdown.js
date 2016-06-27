48f3e45c26177faabf2c7d2c1bb81a92b3767c06
christopher-a-johnson <christopher.a.johnson14@gmail.com>
2016-06-27 09:59:50 -0700

    correction to scroll bar in dropdown

--- src/web/cuc/controls/dropdown/dropdown.js
+++ src/web/cuc/controls/dropdown/dropdown.js
@@ -5,75 +5,79 @@ class Dropdown {
     this.options = options;
     this.reinit();
   }
-  reinit() {
-if(this.options !== undefined){
-    if (Object.keys(this.options).length) {
-      this.options.defaultText = (this.options.defaultText === undefined) ? "Select One" : this.options.defaultText;
-      this.options.itemList = this.options.itemList || [];
-      this.options.sortField = this.options.sortField || this.options.textField || 'text';
-      this.options.sortOrder = this.options.sortOrder || 'asc';
-      this.options.size = this.options.defaultSize || 'medium';
 
-      //Sort dropdown values
-      if (this.options.itemList) {
-        var len = this.options.itemList.length;
-        for (var i = len-1; i>=0; i--){
-          for(var j = 1; j<=i; j++){
-            if(this.options.itemList[j-1][this.options.sortField].toLowerCase() > this.options.itemList[j][this.options.sortField].toLowerCase()){
-              var temp = this.options.itemList[j-1];
-              this.options.itemList[j-1] = this.options.itemList[j];
-              this.options.itemList[j] = temp;
+  reinit() {
+    if (this.options !== undefined) {
+      if (Object.keys(this.options).length) {
+        this.options.defaultText = (this.options.defaultText === undefined) ? "Select One" : this.options.defaultText;
+        this.options.itemList = this.options.itemList || [];
+        this.options.sortField = this.options.sortField || this.options.textField || 'text';
+        this.options.sortOrder = this.options.sortOrder || 'asc';
+        this.options.size = this.options.defaultSize || 'medium';
+
+        //Sort dropdown values
+        if (this.options.itemList) {
+          var len = this.options.itemList.length;
+          for (var i = len - 1; i >= 0; i--) {
+            for (var j = 1; j <= i; j++) {
+              if (this.options.itemList[j - 1][this.options.sortField].toLowerCase() > this.options.itemList[j][this.options.sortField].toLowerCase()) {
+                var temp = this.options.itemList[j - 1];
+                this.options.itemList[j - 1] = this.options.itemList[j];
+                this.options.itemList[j] = temp;
+              }
             }
           }
         }
-      }
 
-      var IconFlag = false;
-      for (var i = 0; i < this.options.itemList.length; i++) {
+        var IconFlag = false;
+        for (var i = 0; i < this.options.itemList.length; i++) {
 
           if (this.options.itemList[i].leftImage != undefined) {
-              IconFlag = true;
-              break;
+            IconFlag = true;
+            break;
           }
-      }
+        }
 
-      this.template = `<button  class="btn dropdown-toggle ${this.options.size}" type="button"  aria-haspopup="true" aria-expanded="true"><span class="selectedText pull-left ${this.options.size}">${this.options.defaultText || '&nbsp;'}</span>
+        this.template = `<button  class="btn dropdown-toggle ${this.options.size}" type="button"  aria-haspopup="true" aria-expanded="true"><span class="selectedText pull-left ${this.options.size}">${this.options.defaultText || '&nbsp;'}</span>
 			<span class="caret"></span>
 			</button>
 			<ul class="dropdown-menu ${this.options.size}" role="menu">
 			${this.options.itemList.map((value, i) =>
-        ` ${value.divider ? '<li class="divider"></li>' : ''}<li value="${value[this.options.valueField || 'value']}" class="${value.class === undefined ? '' : value.class}"><a value="${value[this.options.valueField || 'value']}">${value.leftImage!==undefined? `<img class="em-left-icon" src="${value.leftImage}" />`: (IconFlag === true) ? '<span class="em-left-icon"></span>':''} ${value[this.options.textField|| 'text']}
-       ${value.rightImage!==undefined? `<img class="em-right-icon" src="${value.rightImage}" />`:``}</a> </li>`
+          ` ${value.divider ? '<li class="divider"></li>' : ''}<li value="${value[this.options.valueField || 'value']}" class="${value.class === undefined ? '' : value.class}"><a value="${value[this.options.valueField || 'value']}">${value.leftImage !== undefined ? `<img class="em-left-icon" src="${value.leftImage}" />` : (IconFlag === true) ? '<span class="em-left-icon"></span>' : ''} ${value[this.options.textField || 'text']}
+       ${value.rightImage !== undefined ? `<img class="em-right-icon" src="${value.rightImage}" />` : ``}</a> </li>`
         ).join('') }
 			</ul>`;
-      this.mainParent.innerHTML = this.template;
+        this.mainParent.innerHTML = this.template;
+      }
+      this.menu = this.mainParent.querySelector(".btn");
+      this.init();
+      this.setList();
+      this.setDisabled();
+      this.setReadonly();
+      this.setRestricted();
+      this.setError();
+      this.setDisabledItem();
     }
-    this.menu = this.mainParent.querySelector(".btn");
-    this.init();
-    this.setList();
-    this.setDisabled();
-    this.setReadonly();
-    this.setRestricted();
-    this.setError();
-    this.setDisabledItem();}
   }
+
   init() {
     var self = this;
     self.actions();
     self.menu.setAttribute('tabindex', '0'); // Fix onblur on Chrome
     self.menu.addEventListener('click', self.toggle, false);
-    self.menu.addEventListener('blur', self.close, false);
-    //self.menu.parentNode.addEventListener('click',function(e){ e.stopPropagation();},false);
-    //document.addEventListener('click', self.closeit.bind(self), false);
+    self.menu.addEventListener('blur', self.close, false);  //when we call the blur listener with the close function it is not adding the scroll bar as apart of the div and there for closes.
+    self.menu.parentNode.addEventListener('click', function(e) {
+      e.stopPropagation();
+    }, false);
+    document.addEventListener('click', self.closeit.bind(self), false);
 
-  /*  document.getElementsByTagName('html')[0].addEventListener('keydown',  ( e )=> {
-      if ( e.keyCode === 27 ) {  //Closes dropdown on Esc key
-        self.menu.parentNode.classList.remove('open');
-    }});*/
+    /*  document.getElementsByTagName('html')[0].addEventListener('keydown',  ( e )=> {
+     if ( e.keyCode === 27 ) {  //Closes dropdown on Esc key
+     self.menu.parentNode.classList.remove('open');
+     }});*/
 
     self.menu.parentNode.querySelector('.dropdown-menu').addEventListener('mouseover', (e) => {
       let dropdownSelect = self.menu.parentNode.querySelector('.dropdown-menu li.selected-text');
-
       if (dropdownSelect && dropdownSelect.classList.contains('selected-text')) {
         dropdownSelect.classList.remove('selected-text');
       }
@@ -81,26 +85,26 @@ if(this.options !== undefined){
 
     self.menu.addEventListener('keydown', (e) => {
       let index = 0;
-      if ( e.keyCode === 27 ) {  //Closes dropdown on Esc key
+      if (e.keyCode === 27) {  //Closes dropdown on Esc key
         self.menu.parentNode.classList.remove('open');
-    }
+      }
       else if (e.keyCode === 40) {  // scroll dropdown option using down array key
         let list = self.menu.parentNode.querySelectorAll('.dropdown-menu li');
         while (index < list.length) {
           let item = list[index];
           if (item.classList.contains('selected-text')) {
-           // console.log(index, list.length);
-            if(list.length-1>index){
+            // console.log(index, list.length);
+            if (list.length - 1 > index) {
               item.classList.remove('selected-text');
-              if (list[index+1] && list[index+1] !== null) {
+              if (list[index + 1] && list[index + 1] !== null) {
                 list[index + 1].classList.add('selected-text');
               }
               break;
             }
           }
-            index++;
+          index++;
         }
-          e.preventDefault();
+        e.preventDefault();
       }
 
       else if (e.keyCode === 38) { // scroll dropdown option using up array key
@@ -108,17 +112,17 @@ if(this.options !== undefined){
         let index = 0;
         while (index < list.length) {
           let item = list[index];
-            if (item.classList.contains('selected-text')) {
-            if(index>0){
+          if (item.classList.contains('selected-text')) {
+            if (index > 0) {
               item.classList.remove('selected-text');
 
               list[index - 1].classList.add('selected-text');
               break;
             }
           }
-            index++;
+          index++;
         }
-          e.preventDefault();
+        e.preventDefault();
       }
       else if (e.keyCode === 13) { // select dropdwon value on enter key
         if (!self.menu.parentNode.querySelector('li.selected-text').classList.contains("disabled")) {
@@ -136,15 +140,18 @@ if(this.options !== undefined){
       }
     });
   }
+
   setSelected(value) {
     var self = this;
     self.menu.parentNode.querySelector(".btn .selectedText").innerHTML = value[this.options.textField || 'text'];
-				self.menu.parentNode.querySelector(".btn .selectedText").setAttribute('value', value[this.options.valueField || 'value']);
-				if (!self.menu.parentNode.querySelector(".btn").classList.contains("completed")) {
+    self.menu.parentNode.querySelector(".btn .selectedText").setAttribute('value', value[this.options.valueField || 'value']);
+    if (!self.menu.parentNode.querySelector(".btn").classList.contains("completed")) {
       self.menu.parentNode.querySelector(".btn").classList.add("completed");
-        }
+    }
   }
+
   setList() {
+
     var self = this;
 
     [].forEach.call(self.menu.parentNode.querySelectorAll(".dropdown-menu>li>a"), (value, i) => {
@@ -160,14 +167,14 @@ if(this.options !== undefined){
         if (!self.menu.parentNode.querySelector(".btn").classList.contains("completed")) {
           self.menu.parentNode.querySelector(".btn").classList.add("completed");
         }
-        self.toggle({currentTarget:self.menu});
+        self.toggle({currentTarget: self.menu});
         if (typeof self.options.onChange === 'function') {
           self.options.onChange(e);
         }
       });
       value.addEventListener('mouseover', (e) => {
-        if (e.target.offsetWidth < e.target.scrollWidth)
-        e.target.setAttribute('title',e.target.innerHTML);
+        if (e.target.offsetWidth < e.target.scrollHeight)
+          e.target.setAttribute('title', e.target.innerHTML);
       });
     });
 
@@ -236,38 +243,51 @@ if(this.options !== undefined){
       //self.menu.removeEventListener('blur', self.close);
     });
   }
+
+  closeit(e) {
+    var target = this.menu;
+    console.log('from mouse down');
+    setTimeout(function() {
+      if (target.parentNode && target.parentNode !== null)
+        target.parentNode.classList.remove('open');
+    }, 50);
+  }
+
   actions() {
     var self = this;
 
-    self.toggle = function (e) {
+    self.toggle = function(e) {
       var target = e.currentTarget || e.srcElement;
       target.parentNode.classList.toggle('open');
       var selValue = self.menu.parentNode.querySelector(".btn .selectedText").getAttribute('value');
-      if (selValue === null && self.menu.parentNode.querySelector('.dropdown-menu li:first-child')!==null) {
+      if (selValue === null && self.menu.parentNode.querySelector('.dropdown-menu li:first-child') !== null) {
         self.menu.parentNode.querySelector('.dropdown-menu li:first-child').classList.add('selected-text');
       } else {
-         //var selValue = self.menu.parentNode.querySelector(".btn .selectedText").getAttribute('value');
-         //console.log(self.menu.parentNode.querySelector("li[value='" + selValue + "']"));
-         if(self.menu.parentNode.querySelector("li[value='" + selValue + "']")!==null)
+        //var selValue = self.menu.parentNode.querySelector(".btn .selectedText").getAttribute('value');
+        if (self.menu.parentNode.querySelector("li[value='" + selValue + "']") !== null)
           self.menu.parentNode.querySelector("li[value='" + selValue + "']").classList.add('selected-text');
       }
-      if(e.preventDefault)
+      if (e.preventDefault)
         e.preventDefault();
       return false;
     };
 
-    self.close = function (e) {
-      var target = e.currentTarget || e.srcElement;
-      setTimeout(function () { // links inside dropdown-menu don't fire without a short delay
-        if (target.parentNode && target.parentNode !== null){
-           target.parentNode.classList.remove('open');}
+    //removed function due to blur force closing the menu dropdown on scroll event with use closeit() instead
+    self.close = function(e) {
+      var target = self.menu;
+      console.log('made it here close');
+      setTimeout(function() { // links inside dropdown-menu don't fire without a short delay
+        if (target.parentNode && target.parentNode !== null) {
+          console.log('parent node', target.parentNode);
+          target.parentNode.classList.remove('open');
+        }
       }, 200);
     };
   }
 
   static load() {
     var Dropdowns = document.querySelectorAll('[class="em-dropdown"]');
-    [].forEach.call(Dropdowns, function (item, index) {
+    [].forEach.call(Dropdowns, function(item, index) {
       return new Dropdown(item);
     });
   }
