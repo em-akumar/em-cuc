34261801c2c3193f17ab2fc1b816761e458cd233
Ritesh Dubey <Ritesh.Dubey@elliemae.com>
2016-07-14 18:44:17 +0530

    Added currency-input directive to format input in currency format

--- src/cuc/cuc.angular.js
+++ src/cuc/cuc.angular.js
@@ -781,6 +781,58 @@ cuc.directive('emTxtDecimalUpto', function () {
     };
 });
 
+// Directive for formatting input in currency format.
+cuc.directive('currencyInput', function() {
+    return {
+        restrict: 'A',
+        scope: {
+            field: '=currencyField'
+        },
+        require: "ngModel",
+        link: function (scope, element, attrs, ngModel) {
+          element[0].addEventListener('keyup', function (e) {
+                while (scope.field.charAt(0) == '0') {
+                    scope.field = scope.field.substr(1);
+                }
 
+                scope.field = scope.field.replace(/[^\d.\',']/g, '');
+                var point = scope.field.indexOf(".");
+                if (point >= 0) {
+                    scope.field = scope.field.slice(0, point + 3);
+                }
+
+                var decimalSplit = scope.field.split(".");
+                var intPart = decimalSplit[0];
+                var decPart = decimalSplit[1];
+                intPart = intPart.replace(/[^\d]/g, '');
+                if (intPart.length > 3) {
+                    var intDiv = Math.floor(intPart.length / 3);
+                    while (intDiv > 0) {
+                        var lastComma = intPart.indexOf(",");
+                        if (lastComma < 0) {
+                            lastComma = intPart.length;
+                        }
+                        if (lastComma - 3 > 0) {
+                            intPart = (intPart.slice(0, (lastComma - 3)) + ',' + intPart.slice((lastComma - 3) + Math.abs(0)))
+                        }
+                        intDiv--;
+                    }
+                }
+
+                if (decPart === undefined) {
+                    decPart = "";
+                }
+                else {
+                    decPart = "." + decPart;
+                }
+
+                var res = intPart + decPart;
+                scope.$apply(() => {
+                  scope.field = res
+                });
+            });
+        }
+    };
+});
 
 export {cuc};
